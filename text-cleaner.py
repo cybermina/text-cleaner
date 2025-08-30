d48370d60a3a65a92c8ab83df17b45d5d456b6e8
Baxromova_Nozanin <pozetivmiss@gmail.com>
2025-08-31 00:13:45 +0500

    Create text-cleaner.py

---
+++ text-cleaner.py
@@ -0,0 +1,22 @@
+# Text Cleaner 📝  
+
+This is a simple Python program that removes the word **"bad"** from a given comment.  
+
+## Features  
+- Converts input text to lowercase  
+- Removes unwanted word "bad"  
+- Returns `True` if the text is clean  
+
+## How to Run  
+1. Clone this repository:  
+   ```bash
+   git clone https://github.com/cybermina/text-cleaner.git
+   cd text-cleanner
+2. Run the script:
+   python text_cleaner.py
+EXEMPLE
+Input:
+   This is BAD example
+Output:
+   True
+AUTHOR:Baxromova Nozanin
